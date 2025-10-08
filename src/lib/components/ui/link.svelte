4c09f998683780b6bd246f57dbd90756cecf012e
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-08 14:44:15 +0200

    feat: Add link component

---
+++ src/lib/components/ui/link.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+	import type { Pathname } from '$app/types';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+	type Props = {
+		href: Pathname;
+	} & HTMLAnchorAttributes;
+
+	let { href, children, ...rest }: Props = $props();
+</script>
+
+<a href={resolve(href)} {...rest} aria-current={page.url.pathname === href}>
+	{@render children?.()}
+</a>
