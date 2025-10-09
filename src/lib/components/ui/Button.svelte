cff32935bacc110bb55c68e5792c18d31df13aa0
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-09 09:51:41 +0200

    feat(component): Add button component

---
+++ src/lib/components/ui/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	const { children, class: className, ...rest }: HTMLButtonAttributes = $props();
+</script>
+
+<button {...rest} class={[' rounded-sm px-4 py-2 ', className]}>
+	{@render children?.()}
+</button>
