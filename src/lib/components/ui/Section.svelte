0c3294cd6beae330dc17326cc0725460b614e493
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-09 09:52:26 +0200

    feat(component): Add Section component

---
+++ src/lib/components/ui/Section.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+
+	const { children, class: classes, ...rest }: SvelteHTMLElements['section'] = $props();
+</script>
+
+<section {...rest} class={['grid gap-8 px-5 py-10', classes]}>
+	{@render children?.()}
+</section>
