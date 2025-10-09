4db51c2d211ceca468d8532528237320035a5908
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-09 11:05:59 +0200

    fix(components): Make classes for Link and Button component merge via twMerge

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 
 	const { children, class: className, ...rest }: HTMLButtonAttributes = $props();
 </script>
 
-<button {...rest} class={[' rounded-sm px-4 py-2 ', className]}>
+<button {...rest} class={cn(' rounded-sm px-4 py-2 ', className)}>
 	{@render children?.()}
 </button>
--- src/lib/components/ui/Section.svelte
+++ src/lib/components/ui/Section.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
 	import type { SvelteHTMLElements } from 'svelte/elements';
 
 	const { children, class: classes, ...rest }: SvelteHTMLElements['section'] = $props();
 </script>
 
-<section {...rest} class={['grid gap-8 px-5 py-10', classes]}>
+<section {...rest} class={cn('grid gap-8 px-5 py-10', classes)}>
 	{@render children?.()}
 </section>
