798de183dc03ad059206f2ae56ec54694d6f2dc6
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-14 16:27:32 +0200

    feat(style): Background for upper part of homepage

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -7,6 +7,9 @@
 <div
 	class="bg-linear-to-t to-primary -z-1 absolute inset-0 h-[450px] from-white from-30% to-70%"
 ></div>
+<div
+	class="-z-1 mask-y-from-90% mask-x-from-80% inset-y-130 w-8/10 absolute inset-x-0 mx-auto aspect-video bg-[url($assets/homepattern.webp)] bg-cover bg-no-repeat opacity-80"
+></div>
 <Hero />
 <Information />
 <Welcome />
