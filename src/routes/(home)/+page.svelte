4e39d15a67010c40e0c7735b09a376f9f96dc9b6
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-15 09:59:02 +0200

    fix: Make bg only be 80% width on desktop, 100% on smaller devices

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -8,7 +8,7 @@
 	class="bg-linear-to-t to-primary -z-1 absolute inset-0 h-[450px] from-white from-30% to-70%"
 ></div>
 <div
-	class="-z-1 mask-y-from-90% mask-x-from-80% inset-y-130 w-8/10 absolute inset-x-0 mx-auto aspect-video bg-[url($assets/homepattern.webp)] bg-cover bg-no-repeat opacity-80"
+	class="-z-1 mask-y-from-95% mask-x-from-80% inset-y-130 xl:w-8/10 absolute inset-x-0 mx-auto aspect-video w-full bg-[url($assets/homepattern.webp)] bg-cover bg-no-repeat opacity-80"
 ></div>
 <Hero />
 <Information />
