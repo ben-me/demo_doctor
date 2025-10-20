b410346490b2e30637dbdbd45a4860de866ec38c
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-20 11:12:48 +0200

    refactor(style): Move top background color to all pages, not just home.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,9 @@
 
 <Header />
 <main>
+	<div
+		class="bg-linear-to-t to-primary absolute inset-0 -z-10 h-[450px] from-white from-30% to-70%"
+	></div>
 	{@render children?.()}
 </main>
 <Footer />
