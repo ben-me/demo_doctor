f45040fabc8589cfdd02202c2a838676a14788a0
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-20 11:31:05 +0200

    fix: Change header inline padding

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -54,9 +54,9 @@
 	}
 </script>
 
-<header class="z-10 mx-auto flex w-full max-w-[1920px] justify-between">
+<header class="z-10 mx-auto flex w-full max-w-[1920px] justify-between px-4">
 	<div class="flex gap-12">
-		<Link href="/" class="p-4" aria-label="Zur Demo-Care Homepage">
+		<Link href="/" class="py-4" aria-label="Zur Demo-Care Homepage">
 			<h1>
 				<img src={Logo} alt="Logo der Demo-Care Praxis" class="max-w-[85px]" />
 			</h1>
