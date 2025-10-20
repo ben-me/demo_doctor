8754aa7ee1d45e32057c76a6fc00be9d8b9ea600
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-20 09:59:45 +0200

    fix(style): Adapt service list for desktop view

--- src/routes/(home)/ServiceOverview.svelte
+++ src/routes/(home)/ServiceOverview.svelte
@@ -39,19 +39,26 @@
 
 <Section>
 	<h2 class="text-h3">Unsere Leistungen</h2>
-	{#each SERVICES as service (service)}
-		<article class="border-b-1 grid grid-cols-[auto_1fr] gap-3 border-gray-400/40 py-[0_1.5rem]">
-			<Icon class="w-6 self-center" name={service.icon} />
-			<h3 class="text-h4 col-start-2">
-				{service.name}
-			</h3>
-			<p class="col-start-2">{service.description}</p>
-			<a
-				class="col-start-2 justify-self-end text-right text-sm font-semibold text-blue-700 underline"
-				href={`/leistungen#${service.name.toLocaleLowerCase()}`}
-			>
-				Mehr über {service.name} erfahren
-			</a>
-		</article>
-	{/each}
+	<ul class="flex flex-col flex-wrap gap-3 md:flex-row">
+		{#each SERVICES as service (service)}
+			<li class="max-w-full last-of-type:mx-auto md:flex-[0_1_49%]">
+				<article
+					class="border-b-1 md:border-border grid grid-cols-[auto_1fr] gap-3 border-gray-400/40 bg-white py-[0_1.5rem] md:rounded-sm md:border-0
+					md:p-5 md:shadow-md"
+				>
+					<Icon class="w-6 self-center" name={service.icon} />
+					<h3 class="text-h4 col-start-2">
+						{service.name}
+					</h3>
+					<p class="col-start-2">{service.description}</p>
+					<a
+						class="col-start-2 justify-self-end text-right text-sm font-semibold text-blue-700 underline focus:hover:text-blue-400"
+						href={`/leistungen#${service.name.toLocaleLowerCase()}`}
+					>
+						Mehr über {service.name} erfahren
+					</a>
+				</article>
+			</li>
+		{/each}
+	</ul>
 </Section>
