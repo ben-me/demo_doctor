e1721475e070a5a8e022503512ae118912432248
Ben M <109357901+ben-me@users.noreply.github.com>
2025-10-27 17:12:04 +0100

    fix: Make section become content box on larger screens

--- src/lib/components/ui/Section.svelte
+++ src/lib/components/ui/Section.svelte
@@ -5,6 +5,6 @@
   const { children, class: classes, ...rest }: SvelteHTMLElements['section'] = $props()
 </script>
 
-<section {...rest} class={cn('mx-auto grid max-w-[1920px] gap-8 px-5 py-10', classes)}>
+<section {...rest} class={cn('mx-auto grid max-w-[1920px] lg:box-content gap-8 px-5 py-10', classes)}>
   {@render children?.()}
 </section>
