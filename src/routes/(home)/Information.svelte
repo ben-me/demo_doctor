b3aef192de8ed60d6b39a2dffc2787745187cffa
Ben M <109357901+ben-me@users.noreply.github.com>
2025-11-04 15:47:21 +0100

    refactor: Move mail and phone icon into component

--- src/routes/(home)/Information.svelte
+++ src/routes/(home)/Information.svelte
@@ -1,4 +1,5 @@
 <script lang='ts'>
+  import Icon from '$lib/components/ui/Icon.svelte'
   import Section from '$lib/components/ui/Section.svelte'
 </script>
 
@@ -56,22 +57,12 @@
 
           <div class='text-normal/3'>
             <a href='tel:012345678901' class='text-blue-800 underline'>
-              <svg xmlns='http://www.w3.org/2000/svg' class='inline w-5' viewBox='0 0 24 24'>
-                <path
-                  fill='black'
-                  d='M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24c1.12.37 2.33.57 3.57.57c.55 0 1 .45 1 1V20c0 .55-.45 1-1 1c-9.39 0-17-7.61-17-17c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02z'
-                />
-              </svg>
+              <Icon name='Phone' />
               0123 - 456 789 01
             </a>
             <br />
             <a href='mailto:kontakt@demo-care.de' class='text-blue-800 underline'>
-              <svg xmlns='http://www.w3.org/2000/svg' class='inline w-5' viewBox='0 0 24 24'>
-                <path
-                  fill='black'
-                  d='M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm8-7.175q.125 0 .263-.038t.262-.112L19.6 8.25q.2-.125.3-.312t.1-.413q0-.5-.425-.75T18.7 6.8L12 11L5.3 6.8q-.45-.275-.875-.012T4 7.525q0 .25.1.438t.3.287l7.075 4.425q.125.075.263.113t.262.037'
-                />
-              </svg>
+              <Icon name='Mail' />
               kontakt@demo-care.de
             </a>
           </div>
